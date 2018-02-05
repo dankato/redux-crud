ccb7989699f0a8aa2abad0438aca6d45abf16764
dankato <apearmor@gmail.com>
2018-02-04 17:01:09 -0800

    currently working on unhandled reject in server

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,11 @@ const dbURL = 'mongodb://localhost/reduxcrud';
 
 mongodb.MongoClient.connect(dbURL, function(err, db) {
     app.get('/api/games', (req, res) => {
-        
         db.collection('games').find({}).toArray((err, games) => {
-            res.json({games});
+          res.json({ games });
         });
-    })
+      });
     app.listen(8080, () => console.log('Server is running on localhost:8080'));
-
 });
 
 
